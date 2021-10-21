c75d2abd90a0d9ec060c17e0b4543a496b559781
Fin McCallum <finlaymac@outlook.com>
2021-10-21 15:19:42 +0100

    Get data for one user in usable format

--- src/extract_5000_lines.py
+++ src/extract_5000_lines.py
@@ -6,15 +6,27 @@ import ijson
 inp_file_name = sys.argv[1]
 out_file_name = sys.argv[2]
 
-kinds = {}
-with open(inp_file_name, "rb") as inp:
-    out = ijson.items(inp, 'item.page_hits.item.user')
-    for num, item in enumerate(out):
-        if item in kinds:
-            kinds[item] += 1
-        else:
-            kinds[item] = 1
 
-kinds = dict(sorted(kinds.items(), key=operator.itemgetter(1), reverse=True))
-for k, v in kinds.items():
-    print(k, " : ", v)
+## Generate txts
+# kinds = {}
+# with open(inp_file_name, "rb") as inp:
+#     out = ijson.items(inp, 'item.page_hits.item.user')
+#     for num, item in enumerate(out):
+#         if item in kinds:
+#             kinds[item] += 1
+#         else:
+#             kinds[item] = 1
+# kinds = dict(sorted(kinds.items(), key=operator.itemgetter(1), reverse=True))
+# for k, v in kinds.items():
+#     print(k, " : ", v)
+
+
+with open(inp_file_name, "rb") as inp:
+    out = ijson.items(inp, 'item.page_hits.item')
+    one_user = (o for o in out if o['user'] == "Manakish")
+    for action in one_user:
+        print("{ ", end="")
+        for key in action.keys():
+            if key not in ["cred_hashed_pw", "message", "user", "cred_username"]:
+                print(key + " : "+ action[key], end=", ")
+        print("}")
